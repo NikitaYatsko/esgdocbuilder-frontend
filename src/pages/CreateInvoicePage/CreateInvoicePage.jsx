7f4de69e8209c4422a60435509c5d3b8079e28ed
NikitaYatsko <archon04@mail.ru>
2026-04-12 17:01:56 +0300

    fix

--- src/pages/CreateInvoicePage/CreateInvoicePage.jsx
+++ src/pages/CreateInvoicePage/CreateInvoicePage.jsx
@@ -1,14 +1,14 @@
-import ContentBlock from "@features/auth/components/ContentBlock.jsx";
+
 import {Typography} from "@mui/material";
 
 
 const CreateInvoicePage = () => {
     return (
-            <ContentBlock>
+
                 <Typography sx={{color: "black"}}>
                     Страница смет
                 </Typography>
-            </ContentBlock>
+
     )
 }
 
