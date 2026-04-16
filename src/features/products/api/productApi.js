198d4721bc2c1b28c23cdd445f7910c793d19adf
Gilver1945 <cheban.ivan.2021@bk.ru>
2026-04-16 19:59:14 +0300

    Has been created search and filters on produtch

--- src/features/products/api/productApi.js
+++ src/features/products/api/productApi.js
@@ -1,10 +1,14 @@
 import axiosInstance from "@/shared/api/axiosInstance";
 
 export const productApi = {
-  getAll: (params) => axiosInstance.get("/products", {params}),
+  getAll: (params) => axiosInstance.get("/products", { params }),
+  getAllProducts: (params) => axiosInstance.get("/products/all-products", { params }),
+  search: (term) => axiosInstance.get(`/products/search?q=${encodeURIComponent(term)}`),
+  getByCategory: (id) => axiosInstance.get(`/products/category/${id}`),
+  getByTypeOfUnit: (type) => axiosInstance.get(`/products/type-of-unit/${encodeURIComponent(type)}`),
+  getCategories: () => axiosInstance.get("/products/category"),
   getById: (id) => axiosInstance.get(`/products/${id}`),
   update: (id, data) => axiosInstance.put(`/products/${id}`, data),
   create: (data) => axiosInstance.post("/products", data),
   delete: (id) => axiosInstance.delete(`/products/${id}`),
-   
 };
